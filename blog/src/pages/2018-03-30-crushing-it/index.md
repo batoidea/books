993fe49fe6462673e23db56a3396ccb869fac995
Sejas <antonio@sejas.es>
2018-03-31 00:40:32 +0100

    05 templates /tags y detalle de tags /tags/tag-ejemplo donde se listan todos los posts de ese tag

--- blog/src/pages/2018-03-30-crushing-it/index.md
+++ blog/src/pages/2018-03-30-crushing-it/index.md
@@ -1,12 +1,11 @@
 ---
 path: "/crushing-it"
 date: "2018-03-30T18:41:18.165Z"
-title: "Resumen de Crushing it!"
+title: "Breve resumen de \"Crushing it !\""
 tags: ["Gary Vaynerchuk", "entrepreneurship"]
 excerpt: "Cómo utilizar los mismos trucos y herramientas que los grandes emprendedores utilizan para construir sus negocios y tener influencia."
 ---
 
-# Breve resumen de "*Crushing it !*"
 ## Intro
 El mundo ha cambiado. En la época de nuestros padres, si querías ser el mejor actor tenías que viajar a Hollywood y luchar para destacar. Para tener un gran puesto tenías que estudiar varios masters excesivamente caros y caer bien a la gente adecuada.
 En la actualidad, las redes sociales son una herramienta que cualquier persona puede utilizar para destacar y tener fama. De este modo no importa dónde estés, si tienes talento y trabajas duro conseguirás codearte con los mejores.
--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -3,25 +3,37 @@ import Link from 'gatsby-link'
 
 const IndexPage = ({data}) => {
   const { edges: posts } = data.allMarkdownRemark
-  return <div>
-    {posts.map(({node: post}) => {
-      const { frontmatter } = post
+  return (
+    <div>
+      {posts.map(({node: post}) => {
+        const { frontmatter } = post
 
-      return (
-        <div>
-          <h2>
-            <Link to={frontmatter.path}>
-              {frontmatter.title}
-            </Link>
-          </h2>
-          <p>{frontmatter.date}</p>
-          <p>{frontmatter.excerpt}</p>
-        </div>
-      )
-    })}
-  </div>
+        return (
+          <div>
+            <h2>
+              <Link to={frontmatter.path}>
+                {frontmatter.title}
+              </Link>
+            </h2>
+            <p>{frontmatter.date}</p>
+            <p>{frontmatter.excerpt}</p>
+            <ul>
+              {post.frontmatter.tags.map(tag => {
+                return (
+                  <li>
+                    <Link to={`/tags/${tag}`}>
+                      {tag}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
-
 
 export const query = graphql`
  query IndexQuery {
